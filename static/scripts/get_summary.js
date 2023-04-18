29ae3eca78a8d992c13225aede342f707a8394dc
jackswingle01 <jack774411@gmail.com>
2023-04-18 15:11:23 -0400

    Moved script to separate file

---
+++ static/scripts/get_summary.js
@@ -0,0 +1,19 @@
+
+const get_summary_button = document.querySelector('#get_summary_button');
+const input_textarea = document.querySelector('#input_textarea');
+const output = document.querySelector('#output_p');
+const url = 'http://localhost:5000/summarize'
+get_summary_button.addEventListener('click', async () => {
+    let text = input_textarea.value;
+    let summary = await fetch(url, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            text: text
+        })
+    });
+    let summary_text = await summary.text();
+    output.innerHTML = summary_text;
+});
